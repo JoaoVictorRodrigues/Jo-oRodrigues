0a3004f92d2ca41cfe067633e6dc1d937cee7008
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2018-02-28 14:23:37 -0300

    Incremento 2

---
+++ src/main/java/br/pro/hashi/ensino/desagil/tequilada/Player.java
@@ -0,0 +1,15 @@
+package br.pro.hashi.ensino.desagil.tequilada;
+
+public class Player {
+
+	int row;
+	
+	int col;
+	
+	void printPosition(){
+		
+	System.out.println(row+" "+col);	
+	
+	}
+	
+}
---
+++ src/main/java/br/pro/hashi/ensino/desagil/tequilada/Reder_labirinto.java
@@ -0,0 +1,31 @@
+package br.pro.hashi.ensino.desagil.tequilada;
+import java.io.*;
+
+
+
+public class Reder_labirinto {
+	public static void main (String[]args){
+		try{
+			BufferedReader br = new BufferedReader(new FileReader("labirinto.txt"));
+			while(br.ready()){
+				String linha = br.readLine();
+				char[] linhaX = linha.toCharArray();
+				
+				for(int i=0; i < linhaX.length; i++ ){
+					if (linhaX[i] == '#'){
+						linhaX[i] = 'X';
+						
+					}
+				}
+				System.out.println(linhaX);
+			}
+			br.close();
+		}
+			catch(IOException ioe){
+			ioe.printStackTrace();
+	} 
+  } 
+}
+	
+
+
