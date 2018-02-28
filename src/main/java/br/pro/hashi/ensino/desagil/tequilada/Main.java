1900e8ae1f2caa542d0eb72ab2b3c22fc9d2d05b
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2018-02-28 14:19:06 -0300

    Incremento2

--- src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
+++ src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
@@ -1,9 +1,27 @@
 package br.pro.hashi.ensino.desagil.tequilada;
 
+
 public class Main {
 	
 	public static void main (String[]agrs){
 	
+		Player humanPlayer;
+		
+		humanPlayer = new Player();
+		
+		humanPlayer.row=42;
+		humanPlayer.col=24;
+		humanPlayer.printPosition();
+		
+		
+		Player cpuPlayer;
+		
+		cpuPlayer = new Player();
+		
+		cpuPlayer.row = 99;
+		cpuPlayer.col = 1;
+		cpuPlayer.printPosition();
+		
 	System.out.println("Projeto 1");
 	
 	
